a34022028b480cef51b8d55b2302d68ab12e2970
sunya9 <mugen.xyz@gmail.com>
2020-08-02 01:37:45 +0900

    feat: create app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, act, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  test('render without crash', () => {
+    render(<App />);
+  });
+  test('generate cheat code', async () => {
+    await act(async () => {
+      render(<App />);
+      const inputEl = await screen.findByTestId('input') as HTMLTextAreaElement
+      const outputEl = await screen.findByTestId('output') as HTMLTextAreaElement
+      fireEvent.change(inputEl, { target: { value: 'lorem'}})
+      expect(outputEl.value).toEqual(expect.stringMatching(/^lorem\n\w{8} \w{8}$/))
+      // multiple
+      fireEvent.change(inputEl, { target: { value: 'lorem\nipsum'}})
+      expect(outputEl.value).toEqual(expect.stringMatching(/^lorem\n\w{8} \w{8}\nipsum\n\w{8} \w{8}$/))
+    })
+
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useFormik } from 'formik'
 
+function getRandomInt() {
+  const min = 0
+  const max = 255
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      texts: '',
+    },
+    
+    onSubmit: () => {} // noop
+  })
+  const result = React.useMemo(() => {
+    const texts = formik.values.texts.trim()
+    if(!texts) return ''
+    return texts.split('\n').map((text) => {
+      const code = Array(8).fill('').map(() => getRandomInt().toString(16).padStart(2, '0').toUpperCase()).join('').replace(/(\w{8})/, '$1 ')
+      if(!text) return code
+      return `${text}\n${code}`
+    }).join('\n')
+  }, [formik.values.texts])
+  const tweet = React.useCallback(() => {
+    const w = 550
+    const h = 420
+    const text = `${result}\n\n${window.location.href}`
+    window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`,"_blank","width="+w+",height="+h+",left="+(window.screen.width-w)/2+",top="+(window.screen.height-h)/2+",scrollbars=yes,resizable=yes,toolbar=no,location=yes")
+  }, [result])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>チートコードっぽいのを生成するやつ</h1>
       </header>
+      <main>
+        <label>
+          入力テキスト(改行区切り)
+          <textarea data-testid="input" rows={10} name="texts" value={formik.values.texts} onChange={formik.handleChange} style={{ display: 'block', width: '100%'}} />
+        </label>
+        <h2>生成結果</h2>
+        <textarea data-testid="output" rows={10} readOnly style={{ display: 'block', width: '100%'}} value={result} />
+      </main>
+      <button onClick={tweet}>ツイートする</button>
+      <footer>
+        <hr/>
+        <ul>
+          <li><a href="https://twitter.com/">@_X_y_z_</a></li>
+          <li><a href="https://unsweets.net/">&lt;unsweets/&gt;</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
